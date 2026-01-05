81bfaa1c7d8207fd74a99561186458ad2c930b4a
techpvec15 <ysandeep345@gmail.com>
2026-01-05 08:23:50 +0530

    Update dashboard icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1927759900"
+    serviceWorkerVersion: "573095725"
   }
 });
